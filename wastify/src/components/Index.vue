f6ce89ee6cdd54e1a70677c051030823a850baf7
Karol Wojtulewicz <Wojak27@users.noreply.github.com>
2019-04-02 18:06:16 +0200

    Started to work with the UI

---
+++ wastify/src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <Post/>
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Post";
+export default {
+  name: "Index",
+  components: {
+    Post
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  align-content: center;
+}
+</style>
---
+++ wastify/src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="../assets/logo.png" alt="Wastify" class="logo">
+      <span>Wastify</span>
+      <input
+        type="text"
+        name="search"
+        id
+        class="searchField"
+        placeholder="What are you looking for?"
+      >
+      <a href>Heart</a>
+      <a href>Messages</a>
+      <a href>Profile</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 0px;
+  background-color: #53bc88;
+  height: 50px;
+  border-radius: 0px 0px 20px 20px;
+  align-content: center;
+  top: 0px;
+  position: fixed;
+  width: 100%;
+  left: 0px;
+  vertical-align: bottom;
+  margin: 0 auto;
+  padding: 10;
+  display: -moz-inline-box;
+}
+.navbar span {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+  font-size: 30px;
+  left: 20px;
+  color: #fff;
+}
+
+input.searchField {
+  width: 200px;
+  border-radius: 8px;
+  height: 25px;
+  align-content: center;
+  background-color: white;
+  font-size: 15px;
+}
+
+.logo {
+  height: 40px;
+  left: 0px;
+  align-self: baseline;
+}
+</style>
